026300c78524fb485f2991c83f5e7c7351c89bdd
yryryk <tovchennikov@e1.ru>
2024-01-23 10:58:39 +0500

    feat: add lettersStats

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,53 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import Letters from './components/Letters/Letters'
 import useLetters from './hooks/useLetters'
+import { letters } from "./constants/constants";
 
 function App() {
   const [headline, setHeadline] = useState('Уровень 1');
   const [position, setPosition] = useState(0);
   const [isEnd, setIsEnd] = useState(false);
   const [quantity, setQuantity] = useState(16);
+  const [ lettersOrder, lettersStats, setLettersStats ] = useLetters({
+    lettersRange: 2,
+    lang: 'eng',
+    isNumbers: true,
+    isShifted: false,
+  });
+  function measureTimeInterval() {
+    let prevTime = performance.now();
+    function start() {
+      prevTime = performance.now();
+    };
+    function getTimeInterval() {
+      const time = performance.now();
+      const timeInterval = time - prevTime;
+      prevTime = time;
+      return timeInterval
+    };
+
+    return [start, getTimeInterval]
+  };
+  const [start, getTimeInterval] = measureTimeInterval();
+
   useEffect(() => {
-    const handleKeyPress = () => {
-      if (!isEnd) {
+    function handleKeyPress (evt) {
+      const code = evt.keyCode;
+      if (!isEnd && letters.usedKeyCodes.includes(String(code))) {
         setPosition(state => state + 1);
+        const pressedButton = evt.key;
+        const expectedButton = lettersOrder[position];
+        const stats = lettersStats[expectedButton];
+        stats.button.push(pressedButton);
+        stats.time.push(getTimeInterval());
+        setLettersStats(state => ({...state, [expectedButton]: stats}));
       }
     };
     window.addEventListener("keydown", handleKeyPress);
     return () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
-  }, [isEnd]);
-
-  const [ lettersOrder, lettersStats, setLettersStats ] = useLetters({
-    lettersRange: 1,
-    lang: 'eng',
-    isNumbers: true,
-    isShifted: false,
-  });
+  }, [getTimeInterval, isEnd, lettersOrder, lettersStats, position, setLettersStats]);
   return (
     <div className="App">
       <header className="header">
